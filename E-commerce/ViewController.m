42896ac407081a70cbf42b4178f30bacbba10be7
shuang <364455602@qq.com>
2017-10-25 14:07:12 +0800

    first
    1

---
+++ E-commerce/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  E-commerce
+//
+//  Created by chenlishuang on 2017/10/25.
+//  Copyright © 2017年 chenlishuang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'E-commerce' do
+  # Uncomment the next line if you're using Swift or would like to use dynamic frameworks
+  # use_frameworks!
+
+  # Pods for E-commerce
+
+pod 'AFNetworking'
+pod 'SDWebImage'
+pod 'SVProgressHUD'
+pod 'Masonry'
+pod 'MJRefresh'
+pod 'YYKit'
+pod 'MJExtension'
+pod 'SDCycleScrollView'
+end
